c36a75e904f69528376bbf3de324b2da7a7c2a4e
kagajr <kagajr.hm@gmail.com>
2020-04-06 16:27:51 +0700

    vnote init

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import "./styles/index.scss";
+import store from "./store";
+import App from "./components/App";
+
+render(
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
+  document.getElementById("root")
+);
