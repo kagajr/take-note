ec87f60e5462a71f870352aef99b755077482a3b
kagajr <kagajr.hm@gmail.com>
2020-04-07 23:42:28 +0700

    adding categories

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Dispatch } from 'redux'
+import { connect } from 'react-redux'
 import NoteList from 'containers/NoteList'
 import NoteEditor from 'containers/NoteEditor'
 import Navigation from 'containers/Navigation'
 import AppSideBar from 'containers/AppSidebar'
+import { loadNotes, loadCategories } from 'actions'
 
-const App: React.FC = () => {
+interface AppProps {
+  loadNotes: Function
+  loadCategories: Function
+}
+
+const App: React.FC<AppProps> = ({ loadNotes, loadCategories }) => {
+  useEffect(() => {
+    loadNotes()
+  }, [loadNotes])
   return (
     <div className="app">
       <AppSideBar />
@@ -15,4 +26,11 @@ const App: React.FC = () => {
   )
 }
 
-export default App
+const mapStateToProps = (state) => ({})
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  loadNotes: () => dispatch(loadNotes()),
+  loadCategories: () => dispatch(loadCategories()),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
