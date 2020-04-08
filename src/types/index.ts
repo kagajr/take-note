bd25e225ffff947bb1d29ffbd8955354d25bc560
kagajr <kagajr.hm@gmail.com>
2020-04-08 14:54:12 +0700

    be more expicit with active note and category ids

--- src/types/index.ts
+++ src/types/index.ts
@@ -28,13 +28,13 @@ export interface ApplicationState {
 }
 export interface NoteState {
   notes: NoteItem[]
-  active: string
+  activeNoteId: string
   error: string
   loading: boolean
 }
 export interface CategoryState {
   categories: CategoryItem[]
-  active: string
+  activeCategoryId: string
   error: string
   loading: boolean
 }
